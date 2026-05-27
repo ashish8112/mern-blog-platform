dba0bef009a2b8cc54cc0e660842b94aabe333fd
Ashish <ashish233227shukla@gmail.com>
2026-05-27 12:43:06 +0530

    Added Image

--- mainflow-frontend/src/component/StudentProfile.jsx
+++ mainflow-frontend/src/component/StudentProfile.jsx
@@ -1,9 +1,10 @@
 import {useState} from 'react'
-export default function StudentProfile({name,branch,college})
+export default function StudentProfile({name,branch,college,img})
 {
     const [btn , togglebtn]=useState(false)
     return (
         <div className='profile'>
+        <img src={img}></img>
         <p>Name: {name}</p>
         <br/>
         <p>Branch: {branch}</p>
