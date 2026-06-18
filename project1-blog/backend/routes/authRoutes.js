62af2a0e67da4c4082ca7d768c74de669093ffe3
Ashish <ashish233227shukla@gmail.com>
2026-06-18 19:43:58 +0530

    updated routes

--- project1-blog/backend/routes/authRoutes.js
+++ project1-blog/backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const User = require("../models/User")
+const authMiddleware = require("../middleware/authMiddleware")
 
 
 router.post("/register",async(req,res)=>{
@@ -24,4 +25,77 @@ router.post("/register",async(req,res)=>{
     }
 })
 
+router.post("/login",async(req,res)=>{
+    try{
+        const{email,password}=req.body;
+    const userExists = await User.findOne({email});
+        if(!userExists)
+            return res.status(404).json({message:"Please Enter Correct Email"});
+    const result = await bcrypt.compare(password,userExists.password)
+        if(!result)
+            return res.status(401).json({message:"Please Enter the Correct Password"})
+    const token = jwt.sign({email,id:userExists._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRE})
+    res.status(200).json({message:"welocme"+userExists.name,token,id:userExists._id})
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+})
+
+router.get("/users",authMiddleware,async(req,res)=>{
+    try{
+        const users = await User.find({},{password:false});
+        res.status(200).json(users);
+    }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
+
+router.get("/user/:id",async(req,res)=>{
+    try{
+        const Id = req.params.id;
+        const user = await User.findById(Id,{password:false,createdAt:false});
+        if(!user)
+            return res.status(400).json({message:"User doesn't exist"})
+        return res.status(200).json({user})
+    }
+    catch(err){
+        return res.status(500).json({message:err.message})
+    }
+})
+
+router.put("/user/:id",authMiddleware,async(req,res)=>{
+    try{
+        const Id= req.params.id;
+        const {bio,avatar}=req.body;
+        if(Id!=req.user.id)
+            return res.status(403).json({message:"Not authorized"});
+        if(!bio&&!avatar)
+            return res.status(400).json({message:"Nothing to updatge"});
+        const updateData = {}
+        if(bio) updateData.bio=bio;
+        if(avatar) updateData.avatar=avatar;
+        const updatedUser = await User.findByIdAndUpdate(Id,updateData,{new:true}).select("-password")
+        const changes = [];
+        if(bio) changes.push("bio");
+        if(avatar) changes.push("avatar");
+        return res.status(200).json({message:`updated:${changes.join(", ")}`,updatedUser})
+    }
+    catch(err){
+        return res.status(500).json({message:err.message})
+    }
+})
+router.delete("/user/:id",authMiddleware,async(req,res)=>{
+    try{
+        const Id = req.params.id;
+        if(Id!=req.user.id)
+            return res.status(403).json({message:"Not authorized"});
+        const deletedUser = await User.findByIdAndDelete(Id);
+        return res.status(200).json({message:"User deleted",deletedUser})
+    }
+    catch (err){
+        return res.status(500).json({message:err.message})
+    }
+})
 module.exports=router;
