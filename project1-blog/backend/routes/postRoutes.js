c8cc16f2e4e18fcce0950fe3908d83f659b2cc3c
Ashish <ashish233227shukla@gmail.com>
2026-06-19 15:20:49 +0530

    Implemented get,post,put route

--- project1-blog/backend/routes/postRoutes.js
+++ project1-blog/backend/routes/postRoutes.js
@@ -20,12 +20,49 @@ router.post("/",authMiddleware,async(req,res)=>{
         const {title,content,summary,coverImage,tags,status} = req.body;
         const post = new Post({title,content,summary,coverImage,tags,author:req.user.id,status})
         await post.save();
-        return res.status(201).json({message:"Post "+status})
+        return res.status(201).json({message:"Post "+status+" post id= "+post._id})
     }
     catch(err)
     {
         return res.status(500).json({message:err.message})
     }   
+})
+router.get("/:id",async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const post = await Post.findById(id);
+        if(!post)
+            return res.status(404).json({message:"Post doesn't exists"})
+        res.status(200).json(post);
+    }
+    catch(err){
+        return res.status(500).json({message:err.message})
+    }
+})
+
+router.put("/:id",authMiddleware,async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post)
+            return res.status(404).json({ message: "Post not found" });
+        if(post.author.toString()!=req.user.id)
+            return res.status(403).json({message:"Unauthorized "})
+        const {title, content, summary, coverImage, tags, status} = req.body
+        const updateData = {};
+        if(title)updateData.title=title;
+        if(content)updateData.content=content;
+        if(summary)updateData.summary=summary;
+        if(coverImage)updateData.coverImage=coverImage;
+        if(tags)updateData.tags=tags;
+        if(status)updateData.status=status;
+        if(Object.keys(updateData).length===0)
+            return res.status(400).json({message:"Nothing to update"})
+        const updatePost = await Post.findByIdAndUpdate(post._id,updateData,{new:true})
+        res.status(200).json({message:"Updated",updatePost})
+    }
+    catch(err){
+        return res.status(500).json({message:err.message})
+    }
 })
 
 module.exports=router;
