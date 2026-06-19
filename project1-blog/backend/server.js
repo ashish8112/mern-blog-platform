3187b9c78f84b7955ced993bb05a3ad707986387
Ashish <ashish233227shukla@gmail.com>
2026-06-19 12:24:26 +0530

    integrated post routes

--- project1-blog/backend/server.js
+++ project1-blog/backend/server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors")
 const connectDB = require("./config/db")
-const router = require("./routes/authRoutes")
-
+const authRouter = require("./routes/authRoutes")
+const postRouter = require("./routes/postRoutes");
 const app=express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth",router);
+app.use("/api/auth",authRouter);
+app.use("/api/posts",postRouter)
 app.use((req,res)=>{
     return res.status(404).json({message:"Please Enter correct url"})
 })
