1e5d453a970021009548aa931bef4b7dfc993f6d
Ashish <ashish233227shukla@gmail.com>
2026-06-22 21:21:32 +0530

    Login

--- project1-blog/frontend/src/pages/Login.jsx
+++ project1-blog/frontend/src/pages/Login.jsx
@@ -18,6 +18,7 @@ export default function Login(){
             }
         try{
             await login(formData);
+            alert("Logged In Succesfully");
             navigate("/")
         }
         catch(err){
